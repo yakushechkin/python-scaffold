2606884302d4ff343019b88ad29674cd48c55f8e
yakushechkin <yakushechkin.dmitry@gmail.com>
2021-09-22 19:35:51 +0000

    update hello and test_hello

--- hello.py
+++ hello.py
@@ -1,6 +1,17 @@
-def add(x, y):
-    return x + y
+# def add(x, y):
+#     return x + y
 
 
-result = add(1, 2)
-print(f"This is the sum: {1}, {2}, {result}")
+# result = add(1, 2)
+# print(f"This is the sum: {1}, {2}, {result}")
+
+
+def toyou(x):
+    return f"hi {x}"
+
+def add(x):
+    return x + 1
+
+def subtract(x):
+    return x - 1
+    
--- test_hello.py
+++ test_hello.py
@@ -1,5 +1,25 @@
-from hello import add
+# from hello import add
 
 
-def test_add():
-    assert add(1, 2) == 3
+# def test_add():
+#     assert add(1, 2) == 3
+
+from hello import toyou, add, subtract
+
+
+def setup_function(function):
+    print(f" Running Setup: {function.__name__}")
+    function.x = 10
+
+
+def teardown_function(function):
+    print(f" Running Teardown: {function.__name__}")
+    del function.x
+
+
+### Run to see failed test
+#def test_hello_add():
+#    assert add(test_hello_add.x) == 12
+
+def test_hello_subtract():
+    assert subtract(test_hello_subtract.x) == 9
